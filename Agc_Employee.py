9c61a4c9ac5369bbaf7bd7b7596e15c0e7a4afae
Ahmet Chapraz <ahmet.chapraz@iaau.edu.kg>
2020-05-01 17:56:25 +0600

    make privet

--- Agc_Employee.py
+++ Agc_Employee.py
@@ -1,44 +1,54 @@
 class Employee:
 
     def __init__(self, id, FirstName, LastName, Salary):
-        self.id= id
-        self.FirstName= FirstName
-        self.LastName= LastName
-        self.Salary= Salary
+        self.__id = id
+        self.__FirstName = FirstName
+        self.__LastName = LastName
+        self.__Salary = Salary
+
 
     def __str__(self):
         return "Employee [" + \
-               "id=" + str(self.id) + "," + \
-               "FirstName=" +self.FirstName + "," + \
-               "LastName=" + self.LastName + "," + \
-               "Salary=" + str(self.Salary) + \
+               "id=" + str(self.__id) + "," + \
+               "FirstName=" + self.__FirstName + "," + \
+               "LastName=" + self.__LastName + "," + \
+               "Salary=" + str(self.__Salary) + \
                "]"
 
+
     def getid(self):
-        return self.id
+        return self.__id
+
 
     def getFirstName(self):
-        return self.FirstName
+        return self.__FirstName
+
 
     def getLastName(self):
-        return self.LastName
+        return self.__LastName
+
 
     def getName(self):
-        return self.FirstName + " " + self.LastName
+        return self.__FirstName + " " + self.__LastName
+
 
     def getSalary(self):
-        return self.Salary
+        return self.__Salary
+
 
     def setSalary(self, NewSalary):
-        self.Salary= NewSalary
+        self.__Salary = NewSalary
+
 
     def getAnnualSalary(self):
-        return self.Salary * 12
+        return self.__Salary * 12
+
 
     def raiseSalary(self,percent):
-        self.Salary = self.Salary + self.Salary * (percent/100.0)
+        self.__Salary = self.__Salary + self.__Salary * (percent / 100.0)
 
-Ahmet= Employee(1,"AhmetGurcan","Capraz",1000)
+
+Ahmet= Employee(1, " AhmetGurcan ", " Capraz ", 1000)
 
 print(Employee)
 print(Ahmet.getFirstName())
